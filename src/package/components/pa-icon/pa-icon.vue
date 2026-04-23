3d3d0e37d6a6ab4cf74fe1c7dfae1346a71fbfa6
Cai <caiqi@bbraun.com>
2026-04-23 14:59:23 +0800

    refactor: 优化pa-icon组件的提示内容显示逻辑，支持多语言显示

--- src/package/components/pa-icon/pa-icon.vue
+++ src/package/components/pa-icon/pa-icon.vue
@@ -4,18 +4,25 @@
     <template v-else>
       <pa-popover trigger="hover">
         <template #reference> <span class="m-iconfont m-iconfont_icon" :class="'icon-' + name"></span> </template>
-        {{ tip }}
+        {{ typeof tip === "string" ? tip : tip[languageValue] }}
       </pa-popover>
     </template>
   </section>
 </template>
 
 <script lang="ts" setup>
+import { computed, ComputedRef, inject } from "vue";
 import { ComponentProps } from "./types";
+import { PancakeGlobalConfigType } from "../pa-manager/type";
 
 // # Var
 const props = withDefaults(defineProps<ComponentProps>(), {
   name: "magic_line"
+});
+
+const PancakeGlobalConfig = inject("PancakeGlobalConfig") as ComputedRef<PancakeGlobalConfigType>;
+const languageValue = computed(() => {
+  return PancakeGlobalConfig.value?.language?.value || "zh-CN";
 });
 </script>
 
