303162b6da4c5c98421316fda39990e2b98f04f6
ButlerAi211 <caiqi211@gmail.com>
2026-04-24 11:39:58 +0800

    fix: Color 组件 v1.0.3 规范改造
    - index.ts 单行注释改为 JSDoc 风格
    - pa-color.vue 为所有属性和方法添加 JSDoc 注释
    - pa-color-box.vue 为所有属性和方法添加 JSDoc 注释
    - 修复 ESLint 警告 (prefer-const)

--- src/package/components/pa-color/index.ts
+++ src/package/components/pa-color/index.ts
@@ -1,8 +1,15 @@
-// # Import
+/**
+ * **模块导入**
+ * @description 导入 PaColor 和 PaColorBox 组件
+ * */
 import PaColor from "./pa-color.vue";
 import PaColorBox from "./pa-color-box.vue";
 
-// #Function install
+/**
+ * **组件注册函数**
+ * @param `app` `any` Vue 应用实例
+ * @description 将 PaColor 和 PaColorBox 组件注册到 Vue 应用中
+ * */
 const install = function (app) {
   if (!app._context.components["PaColor"]) {
     app.component("PaColor", PaColor);
