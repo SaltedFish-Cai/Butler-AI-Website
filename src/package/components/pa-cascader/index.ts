5c15d85b624ddf6137977427dfbc67e6cd60a874
Cai <qi.cai@gmail.com>
2026-04-25 20:58:38 +0800

    fix(pa-cascader): 注册 PaCascaderOption 组件

--- src/package/components/pa-cascader/index.ts
+++ src/package/components/pa-cascader/index.ts
@@ -3,13 +3,17 @@
  * @description 导入级联选择器组件
  * */
 import PaCascader from "./pa-cascader.vue";
+import PaCascaderOption from "./pa-cascader-option.vue";
 
 /**
  * **组件注册**
  * @description 注册级联选择器组件
  * */
 const install = function (app) {
-  if (!app._context.components["PaCascader"]) app.component("PaCascader", PaCascader);
+  if (!app._context.components["PaCascader"]) {
+    app.component("PaCascader", PaCascader);
+    app.component("PaCascaderOption", PaCascaderOption);
+  }
 };
 
 export default {
