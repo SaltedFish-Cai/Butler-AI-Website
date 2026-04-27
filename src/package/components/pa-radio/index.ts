ae623f9e3b3f8071cab580569c7e55dcf418cca7
Cai <caiqi@bbraun.com>
2026-04-27 11:04:47 +0800

    fix: 优化组件注册逻辑，移除冗余检查

--- src/package/components/pa-radio/index.ts
+++ src/package/components/pa-radio/index.ts
@@ -18,13 +18,8 @@ import PaRadio from "./pa-radio.vue";
 const install = function (app) {
   if (!app._context.components["PaRadio"]) {
     app.component("PaRadio", PaRadio);
-  }
-  if (!app._context.components["PaRadioItem"]) {
     app.component("PaRadioItem", PaRadioItem);
   }
 };
 
-export default {
-  name: "PaRadio",
-  install
-};
+export default { name: "PaRadio", install };
