45ae31784fac8b4faa256167ce7112b456a30f5c
ButlerAi211 <caiqi211@gmail.com>
2026-04-27 23:26:16 +0800

    fix(tabs): types.d.ts补全visibleMode的@default

--- src/package/components/pa-tabs/types.d.ts
+++ src/package/components/pa-tabs/types.d.ts
@@ -69,8 +69,8 @@ export type ComponentProps = {
 
   /**
    * **隐藏 tabs 方式**
-   * @type `'display'` | `'visible'` | `undefined`
-   * @default `visible`
+   * @type `'display'` | `'visible'`
+   * @default 'visible'
    * @description 当设置该值为 `display` 时，隐藏 tabs 内容元素；`visible` 时不隐藏
    * */
   visibleMode?: ComponentVisibleMode;
@@ -85,31 +85,31 @@ export type ComponentProps = {
 
   /**
    * **样式模式**
-   * @type `'border-card'` | `'card'` | `'default'` | `undefined`
-   * @default `'card'`
+   * @type `'border-card'` | `'card'` | `'default'`
+   * @default `default`
    * @description 预设样式模式：`card` 卡片样式、`default` 默认样式
    * */
   styleMode?: ComponentTabsStyleMode;
 
   /**
    * **对齐方式**
-   * @type `'default'` | `'edge'` | `undefined`
-   * @default `'default'`
+   * @type `'default'` | `'edge'`
+   * @default `default`
    * @description `edge` 边缘对齐、`default` 默认对齐
    * */
   align?: ComponentTabsAlign;
 
   /**
    * **是否使用底线**
-   * @type `boolean` | `undefined`
-   * @default `undefined`
+   * @type `boolean`
+   * @default `false`
    * @description 当设置该值为 `true` 时，使用底线
    * */
   useHeaderLine?: boolean;
 
   /**
    * **是否使用滚动阴影**
-   * @type `boolean` | `undefined`
+   * @type `boolean`
    * @default `true`
    * @description 当设置该值为 `true` 时，使用滚动阴影
    * */
@@ -156,8 +156,8 @@ export type ComponentItemProps = {
 
   /**
    * **是否开启滚动**
-   * @type `boolean` | `undefined`
-   * @default `undefined`
+   * @type `boolean`
+   * @default `true`
    * @description 当设置该值为 `true` 时，开启滚动
    * */
   scroll?: boolean;
@@ -173,7 +173,7 @@ export type ComponentItemProps = {
   /**
    * **懒加载**
    * @type `boolean` | `number` | `undefined`
-   * @default `undefined`
+   * @default `1000`
    * @description `true` 懒加载、`false` 不懒加载、`number` 延迟加载时间（毫秒）
    * */
   lazy?: boolean | number;
@@ -181,23 +181,23 @@ export type ComponentItemProps = {
   /**
    * **是否使用内边距**
    * @type `Array<ComponentPadding>` | `undefined`
-   * @default `undefined`
+   * @default `[]`
    * @description 设置需要添加内边距的方向
    * */
   padding?: Array<ComponentPadding>;
 
   /**
    * **是否使用水平滚动**
-   * @type `boolean` | `undefined`
-   * @default `undefined`
+   * @type `boolean`
+   * @default `false`
    * @description 当设置该值为 `true` 时，使用水平滚动
    * */
   useScrollX?: boolean;
 
   /**
    * **是否使用边框**
-   * @type `boolean` | `undefined`
-   * @default `undefined`
+   * @type `boolean`
+   * @default `false`
    * @description 当设置该值为 `true` 时，使用边框
    * */
   useBorder?: boolean;
@@ -210,7 +210,7 @@ export type ComponentItemProps = {
 export type ComponentLabelProps = {
   /**
    * **插槽内容**
-   * @type `any | undefined`
+   * @type `any` | `undefined`
    * @default `undefined`
    * @description 标签页标题的插槽内容
    * */
@@ -222,11 +222,11 @@ export type ComponentLabelProps = {
    * @default `''`
    * @description 当前选中的标签页标识
    * */
-  activeName?: string;
+  activeName: string;
 
   /**
    * **标签页切换函数**
-   * @type `any | undefined`
+   * @type `any` | `undefined`
    * @default `undefined`
    * @description 切换标签页时调用的回调函数
    * */
@@ -238,5 +238,5 @@ export type ComponentLabelProps = {
    * @default `false`
    * @description 当为 `true` 时，标签页以纵向布局显示
    * */
-  portrait?: boolean;
+  portrait: boolean;
 };
